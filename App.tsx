b1406993b6963727dc3bb2e347bf825735277fc7
Emilio Schepis <emilio.schepis@gmail.com>
2022-03-11 10:39:54 +0100

    feat(auth): implement oauth flow

--- App.tsx
+++ App.tsx
@@ -1,11 +1,65 @@
+import { exchangeCodeAsync, makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Platform, StyleSheet, View } from "react-native";
+
+const auth0ClientId = Constants.manifest?.extra?.auth0ClientId as string;
+const auth0Endpoint = Constants.manifest?.extra?.auth0Endpoint as string;
+
+const useProxy = Platform.select({ web: false, default: true });
+const redirectUri = makeRedirectUri({ useProxy });
 
 export default function App() {
+  const [request, , promptAsync] = useAuthRequest(
+    {
+      redirectUri,
+      clientId: auth0ClientId,
+      responseType: "code",
+      scopes: ["openid", "profile", "email", "offline_access"],
+      extraParams: {
+        audience: "https://emilioschepis.eu.auth0.com/api/v2/",
+      },
+    },
+    {
+      authorizationEndpoint: `${auth0Endpoint}/authorize`,
+    }
+  );
+
+  async function authenticate() {
+    const response = await promptAsync({ useProxy });
+
+    if (response.type !== "success") {
+      if (response.type === "error") {
+        throw new Error(response.error?.message);
+      } else {
+        return;
+      }
+    }
+
+    const verifier = request!.codeVerifier;
+    const code = response.params.code;
+
+    const tokens = await exchangeCodeAsync(
+      {
+        code,
+        clientId: auth0ClientId,
+        redirectUri,
+        extraParams: {
+          grant_type: "authorization_code",
+          code_verifier: verifier ?? "",
+        },
+      },
+      { tokenEndpoint: `${auth0Endpoint}/oauth/token` }
+    );
+
+    // eslint-disable-next-line no-console
+    console.info("Access token:", tokens.accessToken);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button disabled={!request} title="Authenticate" onPress={authenticate} />
     </View>
   );
 }
---
+++ app.config.js
@@ -0,0 +1,37 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "dispensa",
+    slug: "dispensa",
+    version: "1.0.0",
+    scheme: "dispensa",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: false,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      auth0ClientId: process.env.PUBLIC_AUTH0_CLIENT_ID,
+      auth0Endpoint: process.env.PUBLIC_AUTH0_ENDPOINT,
+    },
+  },
+};
